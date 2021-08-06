6cbd2a21bcec75e8929075e8c73ea5f7a513f904
Danny Ayers <danny.ayers@gmail.com>
2021-08-06 16:37:52 +0200

    JSON transformation near enough

---
+++ docs/structures.js
@@ -0,0 +1,46 @@
+var x = {
+  bookmark: { type: 'uri', value: 'http://hyperdata.org/bookmarks/b1' },
+  url: { type: 'uri', value: 'https://en.wikipedia.org/wiki/Exquisite_corpse' },
+  title: { type: 'literal', value: 'Exquisite Corpse' },
+  created: { type: 'literal', value: '2012-04-17T16:06:30Z' },
+  description: {
+    type: 'literal',
+    value: 'Wikipedia page on a surrealist game'
+  },
+  nick: { type: 'literal', value: 'danja' },
+  tag: { type: 'literal', value: 'art' }
+}
+
+var y = {
+  'http://hyperdata.org/bookmarks/b1': {
+    url: 'https://en.wikipedia.org/wiki/Exquisite_corpse',
+    title: 'Exquisite Corpse',
+    created: '2012-04-17T16:06:30Z',
+    description: 'Wikipedia page on a surrealist game',
+    nick: 'danja',
+    tags: ['art', 'game', 'surrealism']
+  }
+}
+
+var z = {
+  'http://hyperdata.org/bookmarks/b1': {
+    url: ['https://en.wikipedia.org/wiki/Exquisite_corpse'],
+    title: ['Exquisite Corpse'],
+    created: ['2021-04-17T16:06:30Z'],
+    description: ['Wikipedia page on a surrealist game'],
+    nick: ['danja'],
+    tag: ['art', 'game', 'surrealism']
+  },
+
+  
+  'http://hyperdata.org/bookmarks/b2': {
+    url: [
+      'https://www.petzl.com/GB/en/Sport/Belay-Devices-And-Descenders/GRIGRI'
+    ],
+    title: ['GriGri'],
+    created: ['2021-045-18T17:07:31Z'],
+    description: ['Belay device with cam-assisted blocking'],
+    nick: ['danja'],
+    tag: ['climbing', 'device']
+  }
+}
